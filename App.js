953ab5f3da26f6316f4dc8474267e03c812e71be
Th@2.dev <99267273+Webww@users.noreply.github.com>
2023-11-16 12:25:26 +0700

    Import express

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-var http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(30);
+app.listen(33, () => {
+    
+});
